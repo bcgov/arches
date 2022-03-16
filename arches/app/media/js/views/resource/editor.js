201448be7683488f3fd8b39c29418fd689c045f7
Ferguson, Brett CSNR:EX <brett@qedsystems.ca>
2022-03-15 22:33:57 -0700

    8247 - Removed hack to get primary descriptor function information, applied black code style to changes

--- arches/app/media/js/views/resource/editor.js
+++ arches/app/media/js/views/resource/editor.js
@@ -32,6 +32,7 @@ define([
     var displayname = ko.observable(data.displayname);
     var resourceId = ko.observable(data.resourceid);
     var appliedFunctions = ko.observable(data['appliedFunctions']);
+    var primaryDescriptorFunction = ko.observable(data['primaryDescriptorFunction']);
     var userIsCreator = data['useriscreator'];
     var creator = data['creator'];
     var selectedTile = ko.computed(function() {
@@ -96,6 +97,7 @@ define([
             cards: data.cards,
             tiles: tiles,
             appliedFunctions: appliedFunctions(),
+            primaryDescriptorFunction: primaryDescriptorFunction(),
             selection: selection,
             scrollTo: scrollTo,
             loading: loading,
